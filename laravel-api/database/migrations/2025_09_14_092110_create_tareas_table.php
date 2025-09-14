19b0dd2a9ba3533b2bcd78ee7551867a2b0185d7
Allan Guamuch <alitang50001@gmail.com>
2025-09-14 03:32:25 -0600

    En este comit hago ver que tuve se hizo una nueva migración en el proyecto de laravel la migración nos servirá para la creación de vista de tareas asignadas al usuarios

---
+++ laravel-api/database/migrations/2025_09_14_092110_create_tareas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id();
+
+            // Relación con la tabla usuarios
+            $table->foreignId('usuario_id')->constrained('usuarios')->cascadeOnDelete();
+
+            $table->string('titulo', 150);
+            $table->text('descripcion')->nullable();
+            $table->enum('estado', ['pendiente', 'en_progreso', 'completada'])->default('pendiente');
+            $table->date('fecha_vencimiento')->nullable();
+            $table->timestamps();
+
+            // Para optimizar búsquedas por usuario
+            $table->index('usuario_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tareas');
+    }
+};
